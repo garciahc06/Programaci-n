ce294b1bb223f20181affa7c1fe10aca908a3155
Andres <garcia.miand22@zaragoza.salesianos.edu>
2023-02-16 12:33:31 +0100

    Eliminados

--- src/SegundaEv/Programacion/Ejercicio17/Bullets.java
+++ src/SegundaEv/Programacion/Ejercicio17/Bullets.java
@@ -1,2 +1,18 @@
-package SegundaEv.Programacion.Ejercicio17;public class bullets {
+package SegundaEv.Programacion.Ejercicio17;
+
+import java.awt.*;
+
+
+public class Bullets extends Rectangle {
+    public Bullets(int posX) {
+        super(posX, 450, 5, 5);
+    }
+    public void paint(Graphics g) {
+        //ponemos color rgb
+        g.setColor(new Color(255, 255, 0));
+        g.fillRect(x, y, width, height);
+    }
+    public void update() {
+        y -= 10;
+    }
 }
--- src/SegundaEv/Programacion/Ejercicio17/Gun.java
+++ src/SegundaEv/Programacion/Ejercicio17/Gun.java
@@ -1,2 +1,21 @@
-package SegundaEv.Programacion.Ejercicio17;public class gun {
+package SegundaEv.Programacion.Ejercicio17;
+
+import java.awt.*;
+
+public class Gun extends Rectangle {
+
+    public Gun() {
+        super(250, 450, 20, 25);
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
+    }
+
+    public void update(int posX) {
+        x = posX - 7;
+        if (x <= 0) x = 0;
+        if (x >= 500) x = 475;
+    }
 }
