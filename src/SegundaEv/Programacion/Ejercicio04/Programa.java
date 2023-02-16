68d9c3b1ddf23b5b72eff71db7666842557d2169
Andres <garcia.miand22@zaragoza.salesianos.edu>
2023-02-16 12:47:38 +0100

    .idea/ dir deleted

--- src/SegundaEv/Programacion/Ejercicio04/Programa.java
+++
@@ -1,9 +0,0 @@
-package SegundaEv.Programacion.Ejercicio04;
-
-public class Programa {
-    public static void main(String[] args) throws Exception {
-        Nif nif1 = new Nif(25209139);
-        nif1.mostrar();
-    }
-}
-
